662688cee57ceb22dba7d9f98e0ac6414de5075f
Parthiv panchal <panchal.parthiv.1903@gmail.com>
2026-05-27 10:18:57 +0530

    Task-2 Completed

---
+++ script.js
@@ -0,0 +1,14 @@
+// Flowly Landing Page JavaScript
+
+console.log("Flowly Landing Page Loaded");
+
+// Example future interactions can go here
+
+// Smooth button click effect
+const buttons = document.querySelectorAll("a");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("Button clicked");
+  });
+});
